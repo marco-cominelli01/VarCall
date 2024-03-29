f63a45168fb119428331dcdc23972f775f74f7b4
marco-cominelli01 <cominellimarco8@gmail.com>
2024-03-29 10:17:29 +0100

    pipeline script

---
+++ pipeline.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+###############################################################################################
+# This script allows to do variant calling starting from the single-end short reads of a trio
+# child-mother-father 
+# The implemented variant caller is freebayes
+###############################################################################################
+# This script is written by Marco Cominelli and Elena Sasso 
+# from Università degli Studi di Milano and Politecnico di Milano
+
+
+# You need 
+# - empty dir
+# - blah blah
+
+
+
+my_wd=/home/BCG_2024_mcominelli/project             # Type here the directory in which all the output folders will be created (make sure                                                    it's empty)
+
+file_dir=/home/BCG2022_genomics_exam                # Here insert the directory in which the fq.gz/index/fasta/BED files are in
+
+
+
+# metti anche dei file dove appendere l'output di errore per ogni programma (usa l'append mode quindi >>)
+
+
+declare -a ar_cases=()  # Array for autosomic recessive cases
+declare -a ad_cases=()  # Array for autosomic dominant cases
+current_param=""        # Parametro corrente
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -ad)
+            current_param="-ad"
+            shift
+            ;;
+        -ar)
+            current_param="-ar"
+            shift
+            ;;
+         -h) 
+	    echo "Parameters are -ad and -ar for autosomic dominant and autosomic 
+	    recessive cases, respectively"
+	    exit 0
+	    ;;
+          *)
+            if [ "$current_param" == "-ar" ]; then
+                ar_cases+=("$1")  # Aggiungi il valore a ar_cases
+            elif [ "$current_param" == "-ad" ]; then
+                ad_cases+=("$1")  # Aggiungi il valore a ad_cases
+            fi
+            shift
+            ;;
+    esac
+done
+
+echo "AR CASES VECTOR: ${ar_cases[@]}"
+
+for ar_trio in ${ar_cases[@]}
+do
+	mkdir "$my_wd/$ar_trio/"
+	mkdir "$my_wd/$ar_trio/fastqc/"
+	mkdir "$my_wd/$ar_trio/bam/"
+	mkdir "$my_wd/$ar_trio/bam/bamcov"
+	mkdir "$my_wd/$ar_trio/qualimap/"
+	mkdir "$my_wd/$ar_trio/multiqc/"
+	mkdir "$my_wd/$ar_trio/vcf/"
+	
+	cd ${my_wd}/${ar_trio}/bam
+
+	bowtie2 -U ${file_dir}/${ar_trio}_father.fq.gz -x ${file_dir}/uni --rg-id 'SF' --rg "SM:father" | samtools view -Sb | samtools sort -o ${ar_trio}_father.bam
+	bowtie2 -U ${file_dir}/${ar_trio}_child.fq.gz -x ${file_dir}/uni --rg-id 'SC' --rg "SM:child" | samtools view -Sb | samtools sort -o ${ar_trio}_child.bam
+        bowtie2 -U ${file_dir}/${ar_trio}_mother.fq.gz -x ${file_dir}/uni --rg-id 'SM' --rg "SM:mother" | samtools view -Sb | samtools sort -o ${ar_trio}_mother.bam
+	
+	samtools index ${ar_trio}_child.bam 
+        samtools index ${ar_trio}_mother.bam
+        samtools index ${ar_trio}_father.bam
+	
+	cd ./bamcov
+
+	bedtools genomecov -ibam ../${ar_trio}_father.bam -bg -trackline -trackopts 'name="father"' -max 100 > ${ar_trio}_fatherCov.bg
+	bedtools genomecov -ibam ../${ar_trio}_mother.bam -bg -trackline -trackopts 'name="mother"' -max 100 > ${ar_trio}_motherCov.bg
+	bedtools genomecov -ibam ../${ar_trio}_child.bam -bg -trackline -trackopts 'name="child"' -max 100 > ${ar_trio}_childCov.bg
+	
+	cd ./../../vcf
+
+	freebayes -f ${file_dir}/universe.fasta -m 20 -C 5 -Q 10 --min-coverage 10 ../bam/${ar_trio}_mother.bam ../bam/${ar_trio}_child.bam ../bam/${ar_trio}_father.bam  > ${ar_trio}.vcf
+
+
+ 	# filtration
+        grep "#" ${ar_trio}.vcf > candilist${ar_trio}.vcf
+	cat ${ar_trio}.vcf| grep -v "##" | python /home/BCG_2024_mcominelli/try "-ar" "high" >> candilist${ar_trio}.vcf
+	
+	# sorting of family member names
+	bcftools query -l candilist${ar_trio}.vcf | sort > samples.txt # extract samples, sort names, save to file
+        bcftools view -S samples.txt candilist${ar_trio}.vcf > ${ar_trio}.sorted.vcf # print samples in “sorted” order to new file
+
+        # bedtools
+        bedtools intersect -a ${ar_trio}.sorted.vcf -b ${file_dir}/targetsPad100.bed -u > ${ar_trio}_candilistTG.vcf
+
+
+
+	# fastqc /home/BCG2022_genomics_exam/$ar_trio* --outdir "$my_wd/$ar_trio/fasqtc"     #FASTQC for all members of TRIO
+	
+
+done
+
+
+
+
+
+
+
+
+
+
+
+

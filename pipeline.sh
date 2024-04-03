14c1927f86bef05062daccf7b07cab53a2d6f85a
marco-cominelli01 <cominellimarco8@gmail.com>
2024-04-03 18:50:41 +0200

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 
 ###############################################################################################
-# This script allows to do variant calling starting from the single-end short reads of a trio
-# child-mother-father 
-# The implemented variant caller is freebayes
+# This script allows to do variant calling starting from the single-end short reads of a trio #
+# child-mother-father.																		  #
+# To do so, the type of the disease (either autosomic dominant -ad or recessive -ar) must     #
+# be assumed and specified at the beginning.												  #
+# The variant caller used is freebayes and is possible to specify the depth at which looking  #
+# for variants: open "greppy.py" with a text editor to find out more.                         #
+# At the end, all the necessary QCs are generated (fastqc, bamqc and multiqc)                 #
 ###############################################################################################
-# This script is written by Marco Cominelli and Elena Sasso 
-# from Università degli Studi di Milano and Politecnico di Milano
+# How to run this script: nohup ./pipeline.sh -ar case178 -ad case210 case221 case240 &       #
+###############################################################################################
+# This script was written by Marco Cominelli and Elena Sasso 								  #
+# from Università degli Studi di Milano and Politecnico di Milano							  #
+###############################################################################################
+
+# You need:
+# 1- An empty directory (see the first line of code)
+# 2- This script (saved in the directory that contains the directory created at point 1)
+# 3- The python script "greppy.py" (saved in the same directory of this script)
 
 
-# You need 
-# - empty dir
-# - blah blah
+# Absolute path of already existing empty directory where to save all the outputs
+my_wd=/home/BCG_2024_mcominelli/project     
+        
+# Absolute path of the directory containing all .fq.gz, genome indexing, genome FASTA and BED files
+file_dir=/home/BCG2022_genomics_exam      
+          
+# Empty array to accomodate autosomic recessive cases
+declare -a ar_cases=()
 
+# Empty array to accomodate autosomic dominant cases  
+declare -a ad_cases=()  
 
-
-my_wd=/home/BCG_2024_mcominelli/project             # Type here the directory in which all the output folders will be created (make sure                                                    it's empty)
-
-file_dir=/home/BCG2022_genomics_exam                # Here insert the directory in which the fq.gz/index/fasta/BED files are in
-
-
-declare -a ar_cases=()  # Array for autosomic recessive cases
-declare -a ad_cases=()  # Array for autosomic dominant cases
-current_param=""        # Parametro corrente
+current_param=""
 
 while [ $# -gt 0 ]; do
     case "$1" in
@@ -35,26 +46,30 @@ while [ $# -gt 0 ]; do
             shift
             ;;
          -h) 
-	    echo "Parameters are -ad and -ar for autosomic dominant and autosomic 
+	    echo "Variant Calling
+		
+		Parameters are -ad and -ar for autosomic dominant and autosomic 
 	    recessive cases, respectively"
 	    exit 0
 	    ;;
           *)
             if [ "$current_param" == "-ar" ]; then
-                ar_cases+=("$1")  # Aggiungi il valore a ar_cases
+                ar_cases+=("$1")
             elif [ "$current_param" == "-ad" ]; then
-                ad_cases+=("$1")  # Aggiungi il valore a ad_cases
+                ad_cases+=("$1") 
             fi
             shift
             ;;
     esac
 done
 
-echo "AR CASES VECTOR: ${ar_cases[@]}"
-echo "AD CASES VECTOR: ${ad_cases[@]}"
+echo "-ar vector contains the cases: ${ar_cases[@]}"
+echo "-ad vector contains the cases: ${ad_cases[@]}"
 
+# Complete analysis for autosomic recessive cases
 for ar_trio in ${ar_cases[@]}
 do
+	# Creation of all the necessary directories
 	mkdir "$my_wd/$ar_trio/"
 	mkdir "$my_wd/$ar_trio/fastqc/"
 	mkdir "$my_wd/$ar_trio/bam/"
@@ -65,93 +80,88 @@ do
 	
 	cd ${my_wd}/${ar_trio}/bam
 
+	# Alignment
 	bowtie2 -U ${file_dir}/${ar_trio}_father.fq.gz -x ${file_dir}/uni --rg-id 'SF' --rg "SM:father" | samtools view -Sb | samtools sort -o ${ar_trio}_father.bam
 	bowtie2 -U ${file_dir}/${ar_trio}_child.fq.gz -x ${file_dir}/uni --rg-id 'SC' --rg "SM:child" | samtools view -Sb | samtools sort -o ${ar_trio}_child.bam
-        bowtie2 -U ${file_dir}/${ar_trio}_mother.fq.gz -x ${file_dir}/uni --rg-id 'SM' --rg "SM:mother" | samtools view -Sb | samtools sort -o ${ar_trio}_mother.bam
+    bowtie2 -U ${file_dir}/${ar_trio}_mother.fq.gz -x ${file_dir}/uni --rg-id 'SM' --rg "SM:mother" | samtools view -Sb | samtools sort -o ${ar_trio}_mother.bam
 	
+	# BAM indexing
 	samtools index ${ar_trio}_father.bam 
-        samtools index ${ar_trio}_child.bam
-        samtools index ${ar_trio}_mother.bam
+    samtools index ${ar_trio}_child.bam
+    samtools index ${ar_trio}_mother.bam
 	
 	cd ./bamcov
 
+	# BAM coverage
 	bedtools genomecov -ibam ../${ar_trio}_father.bam -bg -trackline -trackopts 'name="father"' -max 100 > ${ar_trio}_fatherCov.bg
 	bedtools genomecov -ibam ../${ar_trio}_child.bam -bg -trackline -trackopts 'name="child"' -max 100 > ${ar_trio}_childCov.bg
 	bedtools genomecov -ibam ../${ar_trio}_mother.bam -bg -trackline -trackopts 'name="mother"' -max 100 > ${ar_trio}_motherCov.bg
 
 	cd ./../../vcf
 
+	# Variant calling
 	freebayes -f ${file_dir}/universe.fasta -m 20 -C 5 -Q 10 --min-coverage 10 ../bam/${ar_trio}_mother.bam ../bam/${ar_trio}_child.bam ../bam/${ar_trio}_father.bam  > ${ar_trio}.vcf
 
-
- 	# filtration
-        grep "#" ${ar_trio}.vcf > ${ar_trio}_filtered.vcf
+ 	# Variant prioritization
+    grep "#" ${ar_trio}.vcf > ${ar_trio}_filtered.vcf
 	cat ${ar_trio}.vcf | grep -v "#" | python /home/BCG_2024_mcominelli/try "ar" "standard" >> ${ar_trio}_filtered.vcf
 	
-	# sorting of family member names
+	# Sorting columns by family members name
 	bcftools query -l ${ar_trio}_filtered.vcf | sort > samples.txt     # extract samples, sort names, save to file
-        bcftools view -S samples.txt ${ar_trio}_filtered.vcf > ${ar_trio}_sorted.vcf # print samples in “sorted” order to new file
+    bcftools view -S samples.txt ${ar_trio}_filtered.vcf > ${ar_trio}_sorted.vcf # print samples in “sorted” order to new file
 
-        # bedtools
-        bedtools intersect -a ${ar_trio}_sorted.vcf -b ${file_dir}/targetsPad100.bed -u > ${ar_trio}_final.vcf
-
-
-
-	# fastqc /home/BCG2022_genomics_exam/$ar_trio* --outdir "$my_wd/$ar_trio/fasqtc"     #FASTQC for all members of TRIO
+    # Intersection 
+    bedtools intersect -a ${ar_trio}_sorted.vcf -b ${file_dir}/targetsPad100.bed -u > ${ar_trio}_final.vcf
 	
-
 done
 
-
-
-
+# Complete analysis for autosomic dominant cases
 for ad_trio in ${ad_cases[@]}
 do
-        mkdir "$my_wd/$ad_trio/"
-        mkdir "$my_wd/$ad_trio/fastqc/"
-        mkdir "$my_wd/$ad_trio/bam/"
-        mkdir "$my_wd/$ad_trio/bam/bamcov"
-        mkdir "$my_wd/$ad_trio/qualimap/"
-        mkdir "$my_wd/$ad_trio/multiqc/"
-        mkdir "$my_wd/$ad_trio/vcf/"
+	# Creation of all the necessary directories
+    mkdir "$my_wd/$ad_trio/"
+    mkdir "$my_wd/$ad_trio/fastqc/"
+	mkdir "$my_wd/$ad_trio/bam/"
+    mkdir "$my_wd/$ad_trio/bam/bamcov"
+    mkdir "$my_wd/$ad_trio/qualimap/"
+    mkdir "$my_wd/$ad_trio/multiqc/"
+    mkdir "$my_wd/$ad_trio/vcf/"
 
-        cd ${my_wd}/${ad_trio}/bam
+    cd ${my_wd}/${ad_trio}/bam
+	
+	# Alignment
+    bowtie2 -U ${file_dir}/${ad_trio}_father.fq.gz -x ${file_dir}/uni --rg-id 'SF' --rg "SM:father" | samtools view -Sb | samtools sort -o ${ad_trio}_father.bam
+    bowtie2 -U ${file_dir}/${ad_trio}_child.fq.gz -x ${file_dir}/uni --rg-id 'SC' --rg "SM:child" | samtools view -Sb | samtools sort -o ${ad_trio}_child.bam
+    bowtie2 -U ${file_dir}/${ad_trio}_mother.fq.gz -x ${file_dir}/uni --rg-id 'SM' --rg "SM:mother" | samtools view -Sb | samtools sort -o ${ad_trio}_mother.bam
 
-        bowtie2 -U ${file_dir}/${ad_trio}_father.fq.gz -x ${file_dir}/uni --rg-id 'SF' --rg "SM:father" | samtools view -Sb | samtools sort -o ${ad_trio}_father.bam
-        bowtie2 -U ${file_dir}/${ad_trio}_child.fq.gz -x ${file_dir}/uni --rg-id 'SC' --rg "SM:child" | samtools view -Sb | samtools sort -o ${ad_trio}_child.bam
-        bowtie2 -U ${file_dir}/${ad_trio}_mother.fq.gz -x ${file_dir}/uni --rg-id 'SM' --rg "SM:mother" | samtools view -Sb | samtools sort -o ${ad_trio}_mother.bam
+	# BAM indexing
+    samtools index ${ad_trio}_father.bam
+    samtools index ${ad_trio}_child.bam
+    samtools index ${ad_trio}_mother.bam
 
-        samtools index ${ad_trio}_father.bam
-        samtools index ${ad_trio}_child.bam
-        samtools index ${ad_trio}_mother.bam
+    cd ./bamcov
+	
+	# BAM coverage
+    bedtools genomecov -ibam ../${ad_trio}_father.bam -bg -trackline -trackopts 'name="father"' -max 100 > ${ad_trio}_fatherCov.bg
+    bedtools genomecov -ibam ../${ad_trio}_child.bam -bg -trackline -trackopts 'name="child"' -max 100 > ${ad_trio}_childCov.bg
+    bedtools genomecov -ibam ../${ad_trio}_mother.bam -bg -trackline -trackopts 'name="mother"' -max 100 > ${ad_trio}_motherCov.bg
 
-        cd ./bamcov
+    cd ./../../vcf
 
-        bedtools genomecov -ibam ../${ad_trio}_father.bam -bg -trackline -trackopts 'name="father"' -max 100 > ${ad_trio}_fatherCov.bg
-        bedtools genomecov -ibam ../${ad_trio}_child.bam -bg -trackline -trackopts 'name="child"' -max 100 > ${ad_trio}_childCov.bg
-        bedtools genomecov -ibam ../${ad_trio}_mother.bam -bg -trackline -trackopts 'name="mother"' -max 100 > ${ad_trio}_motherCov.bg
+	# Variant calling
+    freebayes -f ${file_dir}/universe.fasta -m 20 -C 5 -Q 10 --min-coverage 10 ../bam/${ad_trio}_mother.bam ../bam/${ad_trio}_child.bam ../bam/${ad_trio}_father.bam  > ${ad_trio}.vcf
 
-        cd ./../../vcf
+    # Variant prioritization
+    grep "#" ${ad_trio}.vcf > ${ad_trio}_filtered.vcf
+    cat ${ad_trio}.vcf | grep -v "#" | python /home/BCG_2024_mcominelli/try "ad" "standard" >> ${ad_trio}_filtered.vcf
 
-        freebayes -f ${file_dir}/universe.fasta -m 20 -C 5 -Q 10 --min-coverage 10 ../bam/${ad_trio}_mother.bam ../bam/${ad_trio}_child.bam ../bam/${ad_trio}_father.bam  > ${ad_trio}.vcf
+    # Sorting columns by family members name
+    bcftools query -l ${ad_trio}_filtered.vcf | sort > samples.txt     # extract samples, sort names, save to file
+    bcftools view -S samples.txt ${ad_trio}_filtered.vcf > ${ad_trio}_sorted.vcf # print samples in “sorted” order to new file
 
-
-        # filtration
-        grep "#" ${ad_trio}.vcf > ${ad_trio}_filtered.vcf
-        cat ${ad_trio}.vcf | grep -v "#" | python /home/BCG_2024_mcominelli/try "ad" "high" >> ${ad_trio}_filtered.vcf
-
-        # sorting of family member names
-        bcftools query -l ${ad_trio}_filtered.vcf | sort > samples.txt     # extract samples, sort names, save to file
-        bcftools view -S samples.txt ${ad_trio}_filtered.vcf > ${ad_trio}_sorted.vcf # print samples in “sorted” order to new file
-
-        # bedtools
-        bedtools intersect -a ${ad_trio}_sorted.vcf -b ${file_dir}/targetsPad100.bed -u > ${ad_trio}_final.vcf
-
-
-
-        # fastqc /home/BCG2022_genomics_exam/$ad_trio* --outdir "$my_wd/$ad_trio/fasqtc"     #FASTQC for all members of TRIO
-
-
+    # Intersection
+    bedtools intersect -a ${ad_trio}_sorted.vcf -b ${file_dir}/targetsPad100.bed -u > ${ad_trio}_final.vcf
+	
 done
 
 
